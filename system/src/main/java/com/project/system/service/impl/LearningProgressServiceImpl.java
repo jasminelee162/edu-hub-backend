2783eec0ef3a8c60a7678abf94a6e3ffb6848508
jasmine <1556044893@qq.com>
2025-07-02 15:08:17 +0800

    管理员首页图表显示

--- system/src/main/java/com/project/system/service/impl/LearningProgressServiceImpl.java
+++ system/src/main/java/com/project/system/service/impl/LearningProgressServiceImpl.java
@@ -8,6 +8,8 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class LearningProgressServiceImpl extends ServiceImpl<LearningProgressMapper, LearningProgress> implements LearningProgressService {
 
@@ -26,6 +28,23 @@ public class LearningProgressServiceImpl extends ServiceImpl<LearningProgressMap
         } else {
             // 如果不存在，插入新记录
             System.out.println("progress.setId(progress.getId())"+progress.getId());
+            this.save(progress);
+        }
+//        this.updateOrInsertProgressSafe(progress); // 调用你写的安全方法
+
+    }
+
+    // 新增安全版方法，避免多条异常
+    public void updateOrInsertProgressSafe(LearningProgress progress) {
+        List<LearningProgress> list = learningProgressMapper.selectList(
+                new QueryWrapper<LearningProgress>().lambda().eq(LearningProgress::getUserId, progress.getUserId())
+        );
+        LearningProgress existingProgress = list.isEmpty() ? null : list.get(0);
+
+        if (existingProgress != null) {
+            progress.setId(existingProgress.getId());
+            this.updateById(progress);
+        } else {
             this.save(progress);
         }
     }
