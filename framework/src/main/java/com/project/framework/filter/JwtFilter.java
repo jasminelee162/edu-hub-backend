1b530f517655090a7c6f292353b3f33f637bf2ad
董雨欣 <15054035+dongyx2004@user.noreply.gitee.com>
2025-07-02 20:59:43 +0800

    forum bug

--- framework/src/main/java/com/project/framework/filter/JwtFilter.java
+++ framework/src/main/java/com/project/framework/filter/JwtFilter.java
@@ -37,7 +37,7 @@ public class JwtFilter extends BasicHttpAuthenticationFilter {
     */
     private static final List<String> EXCLUDED_URLS = Arrays.asList(
             "/ai","/suggestion","/sandbox/execute","/sandbox/questions","/sandbox/description"
-            ,"/sandbox/title","/user/unread","/template/show"
+            ,"/sandbox/title","/user/unread","/template/show","/document/create"
     );
     @SneakyThrows
     @Override
