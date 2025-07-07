1aec8c4d18eddd1511f67d3f2d73cbea6bdca4b1
cms05 <14641519+cms05@user.noreply.gitee.com>
2025-07-07 13:28:51 +0800

    cms:笔记编辑bug修复

--- admin/src/main/java/com/project/admin/controller/document/DocumentController.java
+++ admin/src/main/java/com/project/admin/controller/document/DocumentController.java
@@ -2,6 +2,7 @@ package com.project.admin.controller.document;
 
 
 import com.project.common.domain.Result;
+import com.project.system.mapper.UserDocumentMapper;
 import com.project.system.service.DocumentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.*;
@@ -22,6 +23,8 @@ public class DocumentController {
     private DocumentService documentService;
     @Autowired
     private SimpMessageSendingOperations messagingTemplate;
+    @Autowired
+    private UserDocumentMapper userDocumentMapper;
 
     //创建共享文档(会返回共享ID)
     @PostMapping("/create")
@@ -61,5 +64,13 @@ public class DocumentController {
         messagingTemplate.convertAndSend(
                 "/topic/document/" + documentId + "/join",
                 users);
+    }
+
+    @GetMapping("/confirm")
+    public Result confirmDocument(@RequestParam("documentId") String documentId) {
+        if(userDocumentMapper.selectById(documentId)==null){
+            return Result.fail("共享文档号不存在");
+        }
+        return Result.success();
     }
 }
